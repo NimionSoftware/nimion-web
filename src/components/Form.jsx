3de27ce7d58ac8095fb522e1bbb09f7cb7dd0a3a
YhanahiRosenthal <yhanahi16@gmail.com>
2023-08-28 19:24:40 -0300

    NW-17: Connection created

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,30 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
+const formSubmit = import.meta.env.FORM_SUBMIT;
+// const front_URL = import.meta.env.FRONT_URL;
+const service = import.meta.env.EMAILJS_SERVICEID;
+const template = import.meta.env.EMAILJS_TEMPLATEID;
+const pKey = import.meta.env.EMAILJS_PUBLICKEY;
 const Form = () => {
-    const formSubmit = import.meta.env.FORM_SUBMIT;
-    const front_URL = import.meta.env.FRONT_URL;
-    const service = import.meta.env.EMAILJS_SERVICEID;
-    const template = import.meta.env.EMAILJS_TEMPLATEID;
-    const pKey = import.meta.env.EMAILJS_PUBLICKEY;
-    const todayDate = new Date();
 
-    const form = useRef();
+    // const form = useRef();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [msg, setMsg] = useState("");
+
+    const templateParams = {
+        from_name: name,
+        from_email: email,
+        from_phone: phone,
+        message: msg
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        emailjs.sendForm(service, template, form.current, pKey)
+        emailjs.send('service_rxracdw', 'template_mrhwz0r', templateParams, 'QyYX6TN8wWVXfqTWX')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
@@ -22,28 +33,21 @@ const Form = () => {
     }
 
     return (
-            <form ref={form} className="flex flex-col items-center gap-4 mt-6" onSubmit={handleSubmit} action={formSubmit} method="POST">
-                <input name="Date" type="hidden" value={todayDate.toLocaleString()} />
+            <form className="flex flex-col items-center gap-4 mt-6" onSubmit={handleSubmit}>
                 <div className="flex items-center w-[70%] h-16 -ml-3">
                     <img className="w-6 h-6 opacity-60" src="../images/user.png" alt="icon user" />
-                    <input name="Full_Name" type="text" placeholder="Nombre completo" className=" w-[85%] ml-3 pl-1 h-10 border-0 border-b border-gray-400 text-sm font-medium outline-none hover:border-secondary-light focus:border-b-2 focus:border-secondary-light" />
+                    <input value={name} onChange={(e) => setName(e.target.value)} name="Full_Name" type="text" placeholder="Nombre completo" className=" w-[85%] ml-3 pl-1 h-10 border-0 border-b border-gray-400 text-sm font-medium outline-none hover:border-secondary-light focus:border-b-2 focus:border-secondary-light" />
                 </div>
                 <div className="flex items-center w-[70%] h-16 -ml-3">
                     <img className="w-6 h-6" src="../images/email.png" alt="icon email" />
-                    <input name="Email" type="email" placeholder="Ingrese Email" className=" w-[85%] ml-3 pl-1 h-10 border-0 border-b border-gray-400 text-sm font-medium outline-none hover:border-secondary-light focus:border-b-2 focus:border-secondary-light" />
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} name="Email" type="email" placeholder="Ingrese Email" className=" w-[85%] ml-3 pl-1 h-10 border-0 border-b border-gray-400 text-sm font-medium outline-none hover:border-secondary-light focus:border-b-2 focus:border-secondary-light" />
                 </div>
                 <div className="flex items-center w-[70%] h-16 -ml-3">
                     <img className="w-6 h-6" src="../images/phone.png" alt="icon user" />
-                    <input name="Phone" type="text" placeholder="Ingrese celular" className=" w-[85%] ml-3 pl-1 h-10 border-0 border-b border-gray-400 text-sm font-medium outline-none hover:border-secondary-light focus:border-b-2 focus:border-secondary-light" />
+                    <input value={phone} onChange={(e) => setPhone(e.target.value)} name="Phone" type="text" placeholder="Ingrese celular" className=" w-[85%] ml-3 pl-1 h-10 border-0 border-b border-gray-400 text-sm font-medium outline-none hover:border-secondary-light focus:border-b-2 focus:border-secondary-light" />
                 </div>
-                <textarea name="Message" className="h-[7.5rem] w-[70%] resize-none border border-gray-400 rounded-lg p-3 mt-3 text-xs font-semibold outline-none focus:border-secondary-light focus:border-2 scrollbar" placeholder="Mensaje"></textarea>
+                <textarea value={msg} onChange={(e) => setMsg(e.target.value)} name="Message" className="h-[7.5rem] w-[70%] resize-none border border-gray-400 rounded-lg p-3 mt-3 text-xs font-semibold outline-none focus:border-secondary-light focus:border-2 scrollbar" placeholder="Mensaje"></textarea>
                 <button type="submit" className="flex justify-center items-center gap-1 text-[.8rem] font-medium text-white bg-[#7683F9] bg-opacity-100 w-[8rem] h-[2.3rem] rounded-sm transition-all mb-1 hover:bg-secondary-light">Enviar iniciativa</button>
-
-                <input type="hidden" name="_next" value={front_URL} />
-                <input type="hidden" name="_captcha" value="false" />
-                <input type="hidden" name="_template" value="box" />
-                <input type="hidden" name="_subject" value="Nimion Client Message!" />
-                <input type="hidden" name="_cc" value="cristianlotorto@gmail.com,yhanahi16@gmail.com" />
             </form>
     )
 }
